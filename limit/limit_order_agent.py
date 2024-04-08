dbe5b95445e5c198e03563d521a70a559b0ce8ea
deeksha parmar <deeksha.parmar@iebrain.com>
2024-04-08 15:00:04 +0530

    add and execute order feature implimented

--- limit/limit_order_agent.py
+++ limit/limit_order_agent.py
@@ -3,14 +3,38 @@ from trading_framework.price_listener import PriceListener
 
 
 class LimitOrderAgent(PriceListener):
-
     def __init__(self, execution_client: ExecutionClient) -> None:
-        """
-
-        :param execution_client: can be used to buy or sell - see ExecutionClient protocol definition
-        """
         super().__init__()
+        self.execution_client = execution_client
+        self.orders = []  # List to store orders
+
+    def add_order(self, is_buy: bool, product_id: str, amount: int, limit: float):
+        """
+        This function used for Adding an order to the agent's list of orders
+        here is_buy Flag indicating whether to buy or sell, product_id: Product ID,
+        amount param indicates Amount to buy/sell, and limit param indicates Limit at which to buy or sell
+        """
+        self.orders.append({'is_buy': is_buy, 'product_id': product_id, 'amount': amount, 'limit': limit})
+
+    def execute_orders(self, product_id: str, price: float):
+        """
+        This function Execute orders if market price is at or better than the limit
+        here  product_id indicating Product ID and price indicating Current market price
+        """
+        for order in self.orders:
+            if order['product_id'] == product_id:
+                if (order['is_buy'] and price <= order['limit']) or (not order['is_buy'] and price >= order['limit']):
+                    if order['is_buy']:
+                        self.execution_client.buy(order['product_id'], order['amount'])
+                    else:
+                        self.execution_client.sell(order['product_id'], order['amount'])
+                    # Remove executed order from list
+                    self.orders.remove(order)
 
     def on_price_tick(self, product_id: str, price: float):
-        # see PriceListener protocol and readme file
-        pass
+        """
+        this function get invoked on market data change
+        here product_id indicated ID of the product that has a price change
+        and price indicates The current market price of the product
+        """
+        self.execute_orders(product_id, price)
--- limit/tests/limit_order_agent_tests.py
+++ limit/tests/limit_order_agent_tests.py
@@ -1,7 +1,40 @@
 import unittest
 
+from limit.limit_order_agent import LimitOrderAgent
+from trading_framework.execution_client import ExecutionClient
+
+
 class LimitOrderAgentTest(unittest.TestCase):
 
-    def test_something(self):
-        self.fail("not implemented")
+    def test_limit_order_execution(self):
+        class MockExecutionClient(ExecutionClient):
+            def __init__(self):
+                super().__init__()
 
+            def buy(self, product_id: str, amount: int):
+                print(f"Bought {amount} shares of {product_id}")
+
+            def sell(self, product_id: str, amount: int):
+                print(f"Sold {amount} shares of {product_id}")
+
+        execution_client = MockExecutionClient()
+        agent = LimitOrderAgent(execution_client)
+
+        # Adding buy order for IBM
+        agent.add_order(is_buy=True, product_id='IBM', amount=1000, limit=100)
+
+        # Price drops below limit, order should execute
+        agent.on_price_tick('IBM', 99.9)
+
+        # Adding sell order for Google
+        agent.add_order(is_buy=False, product_id='GOOG', amount=500, limit=1500)
+
+        # Price rises above limit, order should execute
+        agent.on_price_tick('GOOG', 1510)
+
+        # Both orders should be executed, so the orders list should be empty
+        self.assertEqual(len(agent.orders), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
